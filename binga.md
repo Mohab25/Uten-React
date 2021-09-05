0dc4f64696a8eab92296de1724ebe760b952e793
Mohab25 <homab3@gmail.com>
2021-09-05 15:35:17 +0200

    Updates

---
+++ binga.md
@@ -0,0 +1,14 @@
+## 0.1. This is a Markdown
+
+now i think we are talking *right*
+can you show me how can i move from this place to another one, because it's seems interesting man.
+
+## 0.2. another title goes here
+
+### 0.2.1. and this is a subtite
+
+as i can tell that Magna elit officia ullamco ea eu voluptate consequat proident incididunt culpa et occaecat. Nisi eu quis excepteur deserunt .
+
+### 0.2.2. this is a another subtitle
+
+please Watch the magic happens as you click on the markdown TOC extension to fix the numbering of this last subtitle.
---
+++ precache-manifest.8a067bed9c56a3afb7af9151bdee62b3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e067af95cfe32c857553b93afa41b03",
+    "url": "/Uten-React/index.html"
+  },
+  {
+    "revision": "1301cdc6e4483903c93e",
+    "url": "/Uten-React/static/css/main.22772640.chunk.css"
+  },
+  {
+    "revision": "1b0c7578018b92f793db",
+    "url": "/Uten-React/static/js/2.70cba677.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Uten-React/static/js/2.70cba677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1301cdc6e4483903c93e",
+    "url": "/Uten-React/static/js/main.e479a11b.chunk.js"
+  },
+  {
+    "revision": "493309c0705d9f2d47a0",
+    "url": "/Uten-React/static/js/runtime-main.eb95341f.js"
+  },
+  {
+    "revision": "8fa197e745b398c6eeb9e3eb7195f131",
+    "url": "/Uten-React/static/media/Binga.8fa197e7.png"
+  },
+  {
+    "revision": "75424083585f69330b27f6cbeb07f330",
+    "url": "/Uten-React/static/media/claudio-schwarz-purzlbaum-0-DjV_Tk1cQ-unsplash.75424083.jpg"
+  },
+  {
+    "revision": "5ae23ad29b67289a1375d2043e289c52",
+    "url": "/Uten-React/static/media/fontawesome-webfont.5ae23ad2.eot"
+  },
+  {
+    "revision": "8cca2f02b0af2da365ff4d1755f29146",
+    "url": "/Uten-React/static/media/fontawesome-webfont.8cca2f02.ttf"
+  },
+  {
+    "revision": "b683029bafe0305ac2234038a03e1541",
+    "url": "/Uten-React/static/media/fontawesome-webfont.b683029b.woff"
+  },
+  {
+    "revision": "f99a231ed57ee113b50b1c3e9f9fcdc3",
+    "url": "/Uten-React/static/media/fontawesome-webfont.f99a231e.svg"
+  },
+  {
+    "revision": "c80ed082a2eea6ee0ec2df63a86dceb5",
+    "url": "/Uten-React/static/media/jonny-kennaugh-_NEjh6gzAgo-unsplash.c80ed082.jpg"
+  },
+  {
+    "revision": "50902298371ce0f185167f2f21c1bbab",
+    "url": "/Uten-React/static/media/offer.50902298.jpg"
+  },
+  {
+    "revision": "206c9f5be7a0defee9b4759d1ee4a10d",
+    "url": "/Uten-React/static/media/raphiell-alfaridzy-9dbSMiwTLoo-unsplash.206c9f5b.jpg"
+  },
+  {
+    "revision": "de61bd6d1cf5c99fe0b98f91a1e6dba2",
+    "url": "/Uten-React/static/media/todd-quackenbush-x5SRhkFajrA-unsplash.de61bd6d.jpg"
+  }
+]);
